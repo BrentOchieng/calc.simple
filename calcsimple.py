bb1ec1a6543a342b2b899c65eabedb4931084df3
BrentOchieng <brentwash35@gmail.com>
2025-06-02 12:41:40 +0300

    Update calcsimple.py

--- calcsimple.py
+++ calcsimple.py
@@ -2,7 +2,7 @@
 #asking for user request
 number1=int(input("enter first number: "))
 number2=int(input("enter second number: "))
-operation =input("enter the operation(+,-,*,/)
+operation =input("enter the operation(+,-,*,/")
 
 #operations from the user input
 if operation =="+":
